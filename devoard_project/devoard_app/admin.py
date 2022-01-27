317d7cbda7f4acecae723ad04afa2c97b60777a1
Jiyoung <thdwldud8@gmail.com>
2022-01-28 01:10:51 +0900

    [지영][개발] :: settings.py 수정

--- devoard_project/devoard_app/admin.py
+++ devoard_project/devoard_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import devoard, project, chat
 # Register your models here.
+admin.site.register(devoard)
+admin.site.register(project)
+admin.site.register(chat)
