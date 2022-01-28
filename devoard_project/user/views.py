a70329962625c9eb925558a5952d415f590fa6ed
Jiyoung <thdwldud8@gmail.com>
2022-01-29 00:14:39 +0900

    [지영][BE] :: 세팅 형식 에러

--- devoard_project/user/views.py
+++ devoard_project/user/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from rest_framework import permissions, status, generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer, UserSerializerWithToken
+from rest_framework_jwt.serializers import VerifyJSONWebTokenSerializer
+
+# from google.oauth2 import id_token
+# from google.auth.transport import requests
 
 # Create your views here.
+def current_user(request):
+
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
+
+
+class UserList(APIView):
+
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = UserSerializerWithToken(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def validate_jwt_token(request):
+
+    try:
+        token = request.META['HTTP_AUTHORIZATION']
+        data = {'token': token.split()[1]}
+        valid_data = VerifyJSONWebTokenSerializer().validate(data)
+    except Exception as e:
+        return Response(e)
+
+    return Response(status=status.HTTP_200_OK)
