d1b11ed1f76b32e7a238f9e46698f59ecde39de5
JODAHYE <57217119+JODAHYE@users.noreply.github.com>
2022-02-19 11:23:42 +0900

    surveybutton

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -60,6 +60,7 @@ const Home = () => {
   return (
     <HomeWrapper>
       <RecruitDisplayWrapper>
+        <button><Link to="survey">설문조사</Link></button>
         <RecruitDisplayText>
           <RecruitCnt>{recruitCnt}</RecruitCnt> 팀<br/>
           현재 모집 중
