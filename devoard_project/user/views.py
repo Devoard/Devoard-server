8f0616344de8aad1b13f1cf8dfc6ccc7c2c09da6
Jiyoung <thdwldud8@gmail.com>
2022-02-24 01:53:52 +0900

    [지영][BE] :: github로그인 에러 수정

--- devoard_project/user/views.py
+++ devoard_project/user/views.py
@@ -137,7 +137,7 @@ def github_callback(request):
         response = redirect('home')
         response.set_cookie('git_token',accept_json['token'])
         response.set_cookie('git_userImg',avatar_url)
-        token = Token.objects.get(user=user.id)
+        token = Token.objects.get_or_create(user=user)
         print(token)
         response.set_cookie('token',token)
         response.set_cookie('signin','signin')
