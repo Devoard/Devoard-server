0ecc20c2a105adc030c958d88776a3caf9f9fda4
Jiyoung <thdwldud8@gmail.com>
2022-05-16 23:09:45 +0900

    mypage 수정 구현

--- devoard_project/user/views.py
+++ devoard_project/user/views.py
@@ -207,3 +207,11 @@ class mypage(APIView):
         info = self.get_object(pk)
         serializer = MypageSerializer(info)
         return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        info_update = self.get_object(pk)
+        serializer = UserSerializer(info_update, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
